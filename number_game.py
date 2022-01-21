7b54b1d4b0a211db77044199a67a06b6b6cd8176
Graham Morby <graham.morby@leighton.com>
2022-01-21 13:48:41 +0000

    updated various bits

--- number_game.py
+++ number_game.py
@@ -1,29 +1,45 @@
 import random
 
 
+
 def start_game():
     winner = False
-    user_name = input('Welcome to the number guessing game! What is your name?   ')
+    print('Welcome to my number guessing game!')
+    user_name = input('Can you tell me your name?   ')
     solution = random.randint(0, 100)
+    guesses = 0
 
     while not winner:
-        user_guess = input("We have stored a random number between 0 and 100! What could it be?   ")
+        
+        user_guess = input(
+            "Please tell me your answer, remember it must be between 0 - 100!    ")
         try:
             user_guess = int(user_guess)
             if user_guess > 100 or user_guess < 0:
-                raise ValueError('It can only be as high as 100 or as low as 0 {}, please try again'.format(user_name))
+                raise ValueError(
+                    'It can only be as high as 100 or as low as 0 {}, please try again'.format(user_name))
         except ValueError:
-            print("Sorry that's not quite right! Please use a number between 1 and 100")
+            print(
+                "Remember {} you can only enter a number between 1 - 100".format(user_name))
         else:
             if int(user_guess) < solution:
-                print("That's sadly the wrong answer {}, maybe try a little higher".format(user_name))
+                print("That's sadly the wrong answer {}, maybe try a little higher".format(
+                    user_name))
+                guesses += 1
+                print('You have had {} guesses! Please try again'.format(guesses))
             elif int(user_guess) > solution:
-                print("That's sadly the wrong answer {}, maybe try a little lower".format(user_name))
+                print(
+                    "That's sadly the wrong answer {}, maybe try a little lower".format(user_name))
+                guesses += 1
+                print('You have had {} guesses! Please try again'.format(guesses))
             else:
                 winner = True
-
-    print('Yay! You have found the secret code {}! Welcome to the cool side'.format(user_name))
-    print('Thank you for playing my game...')
-
+                print('Yay! You have found the secret code {}! Welcome to the cool side'.format(user_name))
+                user_replay =  input('Thank you for playing my game! Would you like to play again? Y/N   ')
+                
+                if user_replay.lower() == 'y':
+                    winner = False
+                    solution = random.randint(0, 100)
+                    guesses = 0
 
 start_game()
